c3db8dd071ba39e7492a98ae62044c34dedd8451
ReydVires <ahmad.fire@gmail.com>
2019-03-29 20:46:50 +0700

    add component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Button, TextInput} from 'react-native';
 
 export default class AuthReact extends Component{
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder' };
+  }
+
   render(){
     return(
       <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
         <Text>
           Authentication. Hello!
         </Text>
+        <TextInput placeholder="Fill this blank"></TextInput>
+        <Button title="Click Here"></Button>
       </View>
     );
   }
+
 }
